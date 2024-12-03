2ef2f4f4ccc36b0e050f9663dc5b657793922039
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-12-03 20:42:18 +0530

    fix(server): set default status to 500 in ApiError constructor

--- server/src/lib/api-error.ts
+++ server/src/lib/api-error.ts
@@ -2,7 +2,7 @@ class ApiError extends Error {
     message: string;
     status: number;
 
-    constructor(message: string, status: number) {
+    constructor(message: string, status = 500) {
         super(message);
         this.message = message;
         this.status = status;
