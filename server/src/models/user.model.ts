07d14e051068fae5c96c4f375b209f36ea2da615
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-12-03 20:42:41 +0530

    feat(models): add JWT token generation methods to user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import ms from "ms";
 
 interface IUser extends mongoose.Document {
     email: string;
@@ -7,6 +9,8 @@ interface IUser extends mongoose.Document {
     password: string;
     avatar: string;
     comparePassword: (password: string) => Promise<boolean>;
+    generateAccessToken: () => { accessToken: string; accessExpires: Date };
+    generateRefreshToken: () => { refreshToken: string; refreshExpires: Date };
 }
 
 const userSchema = new mongoose.Schema<IUser>(
@@ -30,6 +34,28 @@ userSchema.pre("save", function (next) {
 userSchema.methods.comparePassword = async function (password: string) {
     return bcrypt.compareSync(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+    const accessToken = jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN_SECRET!, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES
+    });
+
+    const accessExpires = new Date(Date.now() + ms(process.env.ACCESS_TOKEN_EXPIRES!));
+    return { accessToken, accessExpires };
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    const refreshToken = jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN_SECRET!, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES
+    });
+
+    const refreshExpires = new Date(Date.now() + ms(process.env.REFRESH_TOKEN_EXPIRES!));
+    return { refreshToken, refreshExpires };
+};
+
+userSchema.set("toJSON", {
+    transform: (doc, { __v, password, ...rest }, options) => rest
+});
 
 const User = mongoose.model<IUser>("User", userSchema);
 
